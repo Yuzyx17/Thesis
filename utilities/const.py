3c7bc3014362d670e3dab0f76358d5543cf37308
Yuzyx17 <braitboat@gmail.com>
2023-11-11 11:00:43 +0800

    remove testing files

--- utilities/const.py
+++ utilities/const.py
@@ -3,7 +3,7 @@ TESTW, TESTH = 50, 50
 
 from sklearn.svm import SVC
 MODEL_PATH = r'dataset\model\models'
-SCALER_PATH = r'dataset\model\scaler'
+SCALER_PATH = r'dataset\model\scalers'
 FEATURE_PATH = r'dataset\model\features'
 MODEL = SVC(C=10, gamma='scale', kernel='rbf', probability=True, decision_function_shape="ovr")
 
